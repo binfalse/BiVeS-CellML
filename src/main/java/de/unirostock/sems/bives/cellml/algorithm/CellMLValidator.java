56e9bea8e0bee67303caa7101953ecf36ce6beb7
Martin Scharm <martin@binfalse.de>
2014-03-04 22:07:29 +0100

    improved validator, see bives-core:changeset:b739499 and #1

---
+++ src/main/java/de/unirostock/sems/bives/cellml/algorithm/CellMLValidator.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package de.unirostock.sems.bives.cellml.algorithm;
+
+import java.io.File;
+
+import de.unirostock.sems.bives.algorithm.ModelValidator;
+import de.unirostock.sems.bives.cellml.parser.CellMLDocument;
+import de.unirostock.sems.xmlutils.ds.TreeDocument;
+import de.unirostock.sems.xmlutils.tools.XmlTools;
+
+
+/**
+ * @author Martin Scharm
+ *
+ */
+public class CellMLValidator
+	extends ModelValidator
+{
+	
+	/** The doc. */
+	private CellMLDocument doc;
+	
+	
+	/** The error. */
+	private Exception error;
+	
+	/* (non-Javadoc)
+	 * @see de.unirostock.sems.bives.algorithm.ModelValidator#validate(de.unirostock.sems.xmlutils.ds.TreeDocument)
+	 */
+	@Override
+	public boolean validate (TreeDocument d)
+	{
+		try
+		{
+			doc = new CellMLDocument (d);
+		}
+		catch (Exception e)
+		{
+			error = e;
+			return false;
+		}
+		return true;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see de.unirostock.sems.bives.algorithm.ModelValidator#validate(java.io.File)
+	 */
+	@Override
+	public boolean validate (File d)
+	{
+		try
+		{
+			return validate (new TreeDocument (XmlTools.readDocument (d), null));
+		}
+		catch (Exception e)
+		{
+			error = e;
+			return false;
+		}
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see de.unirostock.sems.bives.algorithm.ModelValidator#validate(java.lang.String)
+	 */
+	@Override
+	public boolean validate (String d)
+	{
+		try
+		{
+			return validate (new TreeDocument (XmlTools.readDocument (d), null));
+		}
+		catch (Exception e)
+		{
+			error = e;
+			return false;
+		}
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see de.unirostock.sems.bives.algorithm.ModelValidator#getDocument()
+	 */
+	@Override
+	public CellMLDocument getDocument ()
+	{
+		return doc;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see de.unirostock.sems.bives.algorithm.ModelValidator#getError()
+	 */
+	@Override
+	public Exception getError ()
+	{
+		return error;
+	}
+	
+}
