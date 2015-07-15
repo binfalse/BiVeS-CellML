b14bb7774b420360c94da55cb447e5782663d2fc
Martin Scharm <martin@binfalse.de>
2015-07-15 18:02:34 +0100

    implemented test suite

---
+++ src/test/java/de/unirostock/sems/BivesCellMlTests.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.unirostock.sems;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ * @author Martin Scharm
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestDiffinterpreter.class, TestForChaste.class, TestParser.class })
+public class BivesCellMlTests
+{
+	
+}
